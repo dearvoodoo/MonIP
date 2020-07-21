f7321cf2a253505a2d594e9a750b8c60206d4ae0
VooDoo <dearvoodoo.yt@gmail.com>
2020-07-21 16:48:35 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿﻿$(document).ready( function() {
+    $.getJSON( "https://ipv4.geojs.io/v1/ip.json", function(ipv4){
+        $.getJSON( "https://ipv6.geojs.io/v1/ip.json", function(ipv6){
+            $.getJSON( "https://get.geojs.io/v1/ip/geo.json", function(geo){
+                $("#ipv4").text(ipv4.ip)
+                $("#ipv6").text(ipv6.ip);
+                $("#FAI").text("FAI: "+ geo.organization_name);
+                $("#Localisation").text("Localisation: " + geo.city + "(Lon:" + geo.longitude + ", Lat: " + geo.latitude + ") " + geo.region + " [" + geo.country_code + "] " + geo.country + " " + geo.continent_code);
+            });
+        });
+    });
+});
